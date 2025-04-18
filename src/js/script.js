e89cbc57474434dfb0e79e87f64c43f16bcd2248
anthonyygrant <anthony.hancock1@snhu.edu>
2025-04-17 23:51:21 -0400

    updated src path

--- src/js/script.js
+++ src/js/script.js
@@ -3,48 +3,54 @@ document.addEventListener("DOMContentLoaded", () => {
     const typedText = document.getElementById("typed-text");
     const cursor = document.getElementById("cursor");
     const backToTopButton = document.getElementById("back-to-top");
+    const homeSection = document.getElementById("home");
+  
     let i = 0;
     const speed = 100;
   
-    function typeChar() {
-      if (i < text.length) {
-        typedText.textContent += text.charAt(i);
-        i++;
-        setTimeout(typeChar, speed);
-      } else {
-        setTimeout(() => {
-          cursor.style.animation = "fade-caret 1s ease-out forwards";
-        }, 1500);
+    if (typedText && cursor) {
+      function typeChar() {
+        if (i < text.length) {
+          typedText.textContent += text.charAt(i);
+          i++;
+          setTimeout(typeChar, speed);
+        } else {
+          setTimeout(() => {
+            cursor.style.animation = "fade-caret 1s ease-out forwards";
+          }, 1500);
+        }
       }
+  
+      typeChar();
     }
   
-    // Scroll to home and remove hash from URL
+    // Scroll to top
     window.scrollToHome = () => {
-      document.getElementById("home").scrollIntoView({ behavior: "smooth" });
-      history.pushState(null, null, window.location.pathname);
+      if (homeSection) {
+        homeSection.scrollIntoView({ behavior: "smooth" });
+        history.pushState(null, null, window.location.pathname);
+      }
     };
   
-    const observer = new IntersectionObserver(
-      (entries) => {
-        entries.forEach((entry) => {
-          if (entry.isIntersecting) {
-            backToTopButton.classList.remove("show");
-          } else {
-            backToTopButton.classList.add("show");
-          }
-        });
-      },
-      {
-        root: null,
-        threshold: 0.6,
-      }
-    );
+    // Show/hide Back to Top Button
+    if (backToTopButton && homeSection) {
+      const observer = new IntersectionObserver(
+        (entries) => {
+          entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+              backToTopButton.classList.remove("show");
+            } else {
+              backToTopButton.classList.add("show");
+            }
+          });
+        },
+        {
+          root: null,
+          threshold: 0.6,
+        }
+      );
   
-    const homeSection = document.getElementById("home");
-    if (homeSection) {
       observer.observe(homeSection);
     }
-  
-    typeChar();
   });
   
